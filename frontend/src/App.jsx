6fcfffa92a98eae7bf5fef7734b125c612cb3bd8
lashininimesha30-stack <lashininimesha30@gmail.com>
2026-07-30 18:20:29 +0530

    Add admin complaints page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import FeedbackPage from './pages/resident/FeedbackPage';
 
 //1- import the folder parths for admin pages
 import AdminLogin from './pages/admin/AdminLogin';
-
+import AdminComplaints from './pages/admin/AdminComplaints';
 
 
 
@@ -26,7 +26,8 @@ function App() {
 
         {/*2 - add roots here for admin pages */}
         <Route path="/admin/login" element={<AdminLogin />} />
-        <Route path="/feedback" element={<FeedbackPage />} />
+        <Route path="/admin/complaints" element={<AdminComplaints />} />
+
 
 
         {/*  If the url is incorrect its navigate to complaint page  */}
